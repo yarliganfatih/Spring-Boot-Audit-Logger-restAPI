d49aa1bda72e70c1744526693a0bb2655c073e29
Fatih Yarlıgan <fatih-yarligan@outlook.com>
2024-05-31 02:41:09 +0300

    findByUsername endpoint or you can search with any field

--- src/main/java/com/draft/restapi/repository/UserRepository.java
+++ src/main/java/com/draft/restapi/repository/UserRepository.java
@@ -1,11 +1,26 @@
 package com.draft.restapi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.jpa.repository.Query;
 
 import com.draft.restapi.model.User;
 
 @RepositoryRestResource
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
+    @Query(value = "SELECT * FROM users s WHERE s.username = :username", nativeQuery = true)
+    Page<User> findByUsername (
+        @Param(value = "username") String username,
+        Pageable pageable
+    );
+
+    @RestResource(exported = false)
+    Optional<User> findByUsername(String username);
 }
